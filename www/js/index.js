f436b6b1c1229e8adef950383e52e4e34c8ef5e9
John Raymund Niconi <jraymund.niconi@gmail.com>
2014-02-12 09:11:01 +0800

    change app

--- www/js/index.js
+++ www/js/index.js
@@ -33,8 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        /*app.receivedEvent('deviceready');
-*/		document.location.href = "res/www/index.html";
+        app.receivedEvent('deviceready');
+		document.location.href = "res/app/index.html";
 		
     },
     // Update DOM on a Received Event
