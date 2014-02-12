38b2a525cd17a2f52d03359ff42e448dd03a6584
John Raymund Niconi <jraymund.niconi@gmail.com>
2014-02-12 10:02:11 +0800

    files

--- www/js/index.js
+++ www/js/index.js
@@ -33,8 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-       /* app.receivedEvent('deviceready');*/
-		document.location = "http://www.m.reisandirvys.com/sites/default/files/mobile-application/index.html";
+        app.receivedEvent('deviceready');
+	/*	document.location = "http://www.m.reisandirvys.com/sites/default/files/mobile-application/index.html";*/
 		//document.location.href = "mobile-application/index.html";
 		
     },
